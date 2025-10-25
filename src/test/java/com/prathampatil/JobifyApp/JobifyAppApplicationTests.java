a6dab1df231f24c66bedd7290e146ce041c640bf
Pratham43 <patilpratham359@gmail.com>
2025-10-26 00:56:48 +0530

    JSP Version of the Job App done

---
+++ src/test/java/com/prathampatil/JobifyApp/JobifyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prathampatil.JobifyApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobifyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
